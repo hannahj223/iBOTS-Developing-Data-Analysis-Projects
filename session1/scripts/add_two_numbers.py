4b98c8e45379c802f586c5e099eee97a64ba504e
Hannah Jones <hannahjones223@gmail.com>
2024-02-26 13:49:03 +0100

    new changes

---
+++ session1/scripts/add_two_numbers.py
@@ -0,0 +1,6 @@
+import sys
+
+number1 = float(sys.argv[1])
+number2 = float(sys.argv[2])
+
+print(number1+number2)
---
+++ session1/scripts/timer_30s.py
@@ -0,0 +1,6 @@
+import time
+time.sleep(1) # sleep for 1 second
+
+for i in range(30):
+    print(i +1)
+    time.sleep(1)
